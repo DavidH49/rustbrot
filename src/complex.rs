fb5a40592a4898c06febde33dc1291ba96031d69
DavidH49 <david.heilmannseder@cometomail.de>
2023-12-22 10:33:37 +0100

    Removed unnecessary code in complex.rs and commented complex.rs

--- src/complex.rs
+++ src/complex.rs
@@ -3,7 +3,7 @@ use std::{
     ops
 };
 
-#[derive(PartialEq, PartialOrd)]
+#[derive(Debug, PartialEq, PartialOrd)]
 pub struct Complex {
     re: f64,
     im: f64
@@ -11,31 +11,29 @@ pub struct Complex {
 
 
 impl Complex {
-    // Static functions
+    // Static; Returns a new complex number
     pub fn new(re: f64, im: f64) -> Self {
         Complex { re, im }
     }
 
+    // Static; Returns a complex number with equal real and imaginary parts
     pub fn uniform(x: f64) -> Self {
         Complex { re: x, im: x }
     }
 
+    // Static; Returns complex number (0 + 0i)
     pub fn zero() -> Self {
         Complex::uniform(0.0)
     }
 
-    // Non-Static functions
+    // Non-Static; Returns the absolute value of itself
     pub fn abs(self) -> Complex {
         Complex::new(self.re.abs(), self.im.abs())
-    }
-
-    pub fn magnitude(self) -> f64 {
-        (self.re * self.re + self.im * self.im).sqrt()
     }
 }
 
 
-// Operators
+// Addition Operator
 impl ops::Add<Complex> for Complex {
     type Output = Complex;
 
@@ -45,15 +43,7 @@ impl ops::Add<Complex> for Complex {
 }
 
 
-impl ops::Sub<Complex> for Complex {
-    type Output = Complex;
-
-    fn sub(self, rhs: Complex) -> Self::Output {
-        Complex::new(self.re - rhs.re, self.im - rhs.im)
-    }
-}
-
-
+// Multiplication operator, actually a cross product
 impl ops::Mul<Complex> for Complex {
     type Output = Complex;
 
@@ -80,22 +70,8 @@ impl Copy for Complex {
 
 impl Display for Complex {
     fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
-        display_complex(self)?;
+        let sign = if self.im.is_sign_positive() {"+"} else {"-"};
+        print!("{} {} {}i", self.re, sign, self.im.abs());
         Ok(())
     }
-}
-
-
-impl Debug for Complex {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
-        display_complex(self)?;
-        Ok(())
-    }
-}
-
-
-pub fn display_complex(c: & Complex) -> std::fmt::Result {
-    let sign = if c.im.is_sign_positive() {"+"} else {"-"};
-    print!("{} {} {}i", &c.re, sign, &c.im.abs());
-    Ok(())
 }
